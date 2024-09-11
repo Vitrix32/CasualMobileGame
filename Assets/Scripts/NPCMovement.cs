25debe55f480a530ebec4fa415cb0615254fc58b
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2024-09-11 17:19:18 -0400

    Input system
    Created folder for player input system. Made additional scene changes and changes to scripts. Basic player movement works now.

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -1,20 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class NPCMovement : MonoBehaviour
 {
     public List<GameObject> patrolPath;
-    public float movementSpeed;
+    private int iterator;
+    public GameObject currentNode;
+    private float moveSpeed;
+    private float progress;
+    public bool Patrolling;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = 3;
+        Patrolling = true;
+        iterator = 0;
+        currentNode = patrolPath[iterator];
+        //StartCoroutine("Patrol");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        progress = moveSpeed * Time.deltaTime;
+    }
+
+    IEnumerator Patrol()
+    {
+        while (Patrolling) 
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, currentNode.transform.position, progress);
+            if (Vector3.Distance(this.transform.position, currentNode.transform.position) < 0.001f)
+            {
+                if (iterator == patrolPath.Count)
+                {
+                    iterator = 0;
+                    currentNode = patrolPath[iterator];
+                }
+                else
+                {
+                    iterator++;
+                    currentNode = patrolPath[iterator];
+                }
+            }
+        }
+        return null;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public Vector2 moveVector;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveVector = Vector2.zero;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        moveLogic();
+    }
+
+    private void moveLogic()
+    {
+        Vector2 val = moveVector * moveSpeed * Time.fixedDeltaTime;
+        Debug.Log(val);
+        rb.velocity = val;
+    }
+
+    private void OnMovement(InputValue val)
+    {
+        moveVector = val.Get<Vector2>();
     }
 }
