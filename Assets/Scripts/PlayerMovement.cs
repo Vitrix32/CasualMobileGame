abfb8bd2937e1cf0123a7476cb1bd05f2ad27bd8
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2024-09-21 20:53:50 -0400

    Touchscreen joystick
    Added a fully functional touchscreen joystick into my test scene.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    private Vector2 moveVector;
+    [SerializeField]
+    private GameObject joystick;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Vector2 joystickSize;
+
+    private Finger movementFinger;
+    private GameObject knob;
     private Rigidbody2D rb;
+    private Vector2 moveVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        moveVector = Vector2.zero;
+        knob = GameObject.Find("JoystickKnob");
         rb = this.GetComponent<Rigidbody2D>();
+        moveVector = Vector2.zero;
     }
 
     private void FixedUpdate()
@@ -37,5 +49,61 @@ public class PlayerMovement : MonoBehaviour
     private void OnInteract()
     {
         //do thing
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += HandleFingerDown;
+        ETouch.Touch.onFingerUp += HandleFingerUp;
+        ETouch.Touch.onFingerMove += HandleFingerMove;
+    }
+
+    private void OnDisable()
+    {
+        ETouch.Touch.onFingerDown -= HandleFingerDown;
+        ETouch.Touch.onFingerUp -= HandleFingerUp;
+        ETouch.Touch.onFingerMove -= HandleFingerMove;
+        EnhancedTouchSupport.Disable();
+    }
+
+    private void HandleFingerDown(Finger playerFinger)
+    {
+        if (playerFinger != null && RectTransformUtility.RectangleContainsScreenPoint(joystick.GetComponent<RectTransform>(), playerFinger.screenPosition))
+        {
+            movementFinger = playerFinger;
+            moveVector = Vector2.zero;
+        }
+    }
+
+    private void HandleFingerUp(Finger playerFinger)
+    {
+        if (playerFinger == movementFinger)
+        {
+            movementFinger = null;
+            knob.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            moveVector = Vector2.zero;
+        }
+    }
+
+    private void HandleFingerMove(Finger playerFinger)
+    {
+        if (playerFinger == movementFinger)
+        {
+            Vector2 knobPosition;
+            float maxMovement = joystickSize.x / 2.0f;
+
+            if (Vector2.Distance(playerFinger.currentTouch.screenPosition, joystick.GetComponent<RectTransform>().anchoredPosition) > maxMovement)
+            {
+                knobPosition = (playerFinger.currentTouch.screenPosition - joystick.GetComponent<RectTransform>().anchoredPosition).normalized * maxMovement;
+            }
+            else
+            {
+                knobPosition = playerFinger.currentTouch.screenPosition - joystick.GetComponent<RectTransform>().anchoredPosition;
+            }
+
+            knob.GetComponent<RectTransform>().anchoredPosition = knobPosition;
+            moveVector = knobPosition / maxMovement;
+        }
     }
 }
