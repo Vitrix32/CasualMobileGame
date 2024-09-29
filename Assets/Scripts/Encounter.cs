345a09e77eac4d2b17dcc4db3e41b5b2049726ba
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2024-09-28 23:54:57 -0400

    Encounters
    There is now an encounter object that can be placed in a level to send the player to the combat scene. Need to iron out the details of the transition. Also created a new folder for scripts and made some changes to my test scene. The encounter object will disappear once activated and the player moves a certain distance away from it, but you are currently unable to   return from combat to the main game world.

--- Assets/Scripts/Encounter.cs
+++ Assets/Scripts/Encounter.cs
@@ -1,28 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Encounter : MonoBehaviour
 {
+    private GameObject player;
     private int enemyAmount;
+    public bool encountered;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
+        encountered = false;
         //Add code to randomize later
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (encountered && Vector3.Distance(this.transform.position, player.transform.position) > 20.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        this.GetComponent<Collider2D>().enabled = false;
-        GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
-        SceneManagement.DontDestroyOnLoad(GameObject.Find("Player"));
-        SceneManager.LoadScene(0);
+        if (!encountered && collision == player.GetComponent<Collider2D>())
+        {
+            encountered = true;
+            player.GetComponent<PlayerMovement>().enabled = false;
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            SceneManagement.DontDestroyOnLoad(player);
+            SceneManagement.DontDestroyOnLoad(this.gameObject);
+            SceneManager.LoadScene("BattleScene");
+        }
     }
 }
