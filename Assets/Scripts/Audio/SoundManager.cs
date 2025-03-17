1593c95338ee42a592768ca39b887505d45f7d5f
youkalele <64647884+youkalele@users.noreply.github.com>
2025-03-16 23:29:18 -0400

    Desert tilemap improvements and volume sliders

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -14,10 +14,19 @@ public class SoundManager : MonoBehaviour
         {
             PlayerPrefs.SetFloat("Volume", 1);
             LoadVolume();
+            PlayerPrefs.SetFloat("MusicVolume", 1);
+            LoadMusicVolume();
+            PlayerPrefs.SetFloat("SFXVolume", 1);
+            LoadSFXVolume();
+            PlayerPrefs.SetFloat("UIVolume", 1);
+            LoadUIVolume();
         } 
         else
         {
             LoadVolume();
+            LoadMusicVolume();
+            LoadSFXVolume();
+            LoadUIVolume();
         }
     }
 
@@ -36,5 +45,62 @@ public class SoundManager : MonoBehaviour
     private void LoadVolume()
     {
         VolumeSlider.value = PlayerPrefs.GetFloat("Volume");
+    }
+
+    //music
+    public void ChangeMusicVolume()
+    {
+        //Not Audio Listener
+        GameObject player = GameObject.Find("WorldPlayer");
+        player.GetComponent<UniversalAudioHandling>().ChangeSourceVolume("Music", VolumeSlider.value);
+        SaveMusicVolume();
+    }
+
+    private void SaveMusicVolume()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", VolumeSlider.value);
+    }
+
+    private void LoadMusicVolume()
+    {
+        VolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+
+    //effects
+    public void ChangeSFXVolume()
+    {
+        //Not Audio Listener
+        GameObject player = GameObject.Find("WorldPlayer");
+        player.GetComponent<UniversalAudioHandling>().ChangeSourceVolume("SFX", VolumeSlider.value);
+        SaveSFXVolume();
+    }
+
+    private void SaveSFXVolume()
+    {
+        PlayerPrefs.SetFloat("SFXVolume", VolumeSlider.value);
+    }
+
+    private void LoadSFXVolume()
+    {
+        VolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
+
+    //UI
+    public void ChangeUIVolume()
+    {
+        //Not Audio Listener
+        GameObject player = GameObject.Find("WorldPlayer");
+        player.GetComponent<UniversalAudioHandling>().ChangeSourceVolume("UI", VolumeSlider.value);
+        SaveUIVolume();
+    }
+
+    private void SaveUIVolume()
+    {
+        PlayerPrefs.SetFloat("UIVolume", VolumeSlider.value);
+    }
+
+    private void LoadUIVolume()
+    {
+        VolumeSlider.value = PlayerPrefs.GetFloat("UIVolume");
     }
 }
