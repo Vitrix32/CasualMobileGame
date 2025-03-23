bd33b84e242f2141ab8405e35abc288e797e49fc
joelward2025 <joelward@cedarville.edu>
2025-03-23 18:01:55 -0400

    soldier blocks cave entrance

---
+++ Assets/Scripts/CaveSoldierMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CaveSoldierMovement : MonoBehaviour
+{
+    private GameObject dialogueManager;
+    private int currentStep;
+
+    void Start()
+    {
+        dialogueManager = this.GetComponent<NPCDialogue>().dialoguemanager;
+        currentStep = 0;
+    }
+
+    void Update()
+    {
+        // Check for specific dialogue value
+        if (dialogueManager.GetComponent<DialogueManager>().GetNPCValue("Jeff") == 5 && currentStep == 0)
+        {
+            currentStep++;
+            // Start movement
+            this.GetComponent<NPCMovement>().startPatrol();
+        }
+    }
+}
