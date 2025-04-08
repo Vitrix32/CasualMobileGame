224898b8f67c2d96e7da64b45aa715422344e5eb
SomeoneYouDontKnow42 <m_carter543@yahoo.com>
2025-04-07 20:08:41 -0400

    Priest, Statue, and Dialogue fixes
    1. Removed unused dialogue, objects, and quests related to the scrapped Jeff Scroll Quest.
    2. Made Cat not appear in the starting forest area so the lore guy can explain it and the cat doesn't have issues when joining the party
    3. Made lore guy explain stuff
    4. Made statue in town disappear when it's supposed to
    5. Enabled the canvas
    6. Gave LoreSlate a bible verse (James 1:12 NIV)
    7. Put Priest in church and made him move (still couldn't find a priest sprite though)

---
+++ Assets/Scripts/StatueMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DesertChestMovement : MonoBehaviour
+{
+    private GameObject dialogueManager;
+    private int currentStep;
+
+    void Start()
+    {
+        dialogueManager = this.GetComponent<NPCDialogue>().dialoguemanager;
+        currentStep = 0;
+    }
+
+    void Update()
+    {
+        // Check for specific dialogue value
+        if (dialogueManager.GetComponent<DialogueManager>().GetNPCValue("DungeonChest") >= 1 && currentStep == 0)
+        {
+            currentStep++;
+            // Start movement
+            this.GetComponent<NPCMovement>().startPatrol();
+        }
+    }
+}
